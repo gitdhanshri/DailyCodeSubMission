f6affced84e77a1f74c6a9c90dc5b3cc7b9214e4
gitdhanshri <2022bit049@sggs.ac.in>
2024-04-21 23:46:49 +0530

    Another Example of ThrowCatchBlock

---
+++ userThrownCatchBlock.java
@@ -0,0 +1,11 @@
+class Demo{
+	public static void main(String args[]){
+		Demo d1=new Demo();
+		try{
+			FileOutputStream fos=new  FileOutputStream("Abc.txt");
+		}
+		catch(ArithmeticException e){
+			
+		}
+	}
+}
