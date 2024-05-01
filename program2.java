7568d5e66bc6c09bdfbfdc573372d950f233334a
gitdhanshri <2022bit049@sggs.ac.in>
2024-05-02 00:37:51 +0530

    Use Of OwnCreatyed Package

---
+++ program2.java
@@ -0,0 +1,10 @@
+//import packadditon.Addition;
+class Program2{
+	public static void main(String args[]){
+		//creating an Addition class Object
+		packadditon.Addition add_obj=new packadditon.Addition(20,13.5);
+		//call sum method
+		add_obj.sum();
+		add_obj.Subtract(10,5);
+	}
+}
