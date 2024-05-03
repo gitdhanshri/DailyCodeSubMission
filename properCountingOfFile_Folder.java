c5493694b82a2adc26425841bdc04a7858b3bc25
gitdhanshri <2022bit049@sggs.ac.in>
2024-05-03 17:53:25 +0530

    Count File and Folder

--- properCountingOfFile_Folder.java
+++ properCountingOfFile_Folder.java
@@ -2,38 +2,113 @@ import java.io.*;
 class CountFile_Folder{
 	int countFile=0;
 	int countFolder=0;
-void countMethod1(String path)throws ownException{
+	int countEndsWithJava=0;
+	int countEndsWithClass=0;
+/*int countFolder(String path)throws ownException{
 	try{
 		File f_name=new File(path);
 		File[] listOfAllFile=f_name.listFiles();
-	  	//System.out.println("\nList Of all Files");
 		for(int i=0;i<listOfAllFile.length;i++){
 			boolean res=listOfAllFile[i].isDirectory();
+			boolean res2=listOfAllFile[i].isFile();
 			if(res==true){
 			//System.out.println(i+"->Directory-:>"+listOfAllFile[i]);
-				countMethod1("listOfAllFile[i]");
-				countFolder++;
+			String s=listOfAllFile[i].getPath();
+
+				 countFolder=countFolder+1;
+				countFolder(s);
 			}
-			else{
+			else if(res2==true){
 				//System.out.println(i+"->File-:>"+listOfAllFile[i]);
 				countFile++;	
 			}
+			else{
+				return 0;	
+			}
 		}
-		//System.out.println("Number of Files-->"+countFile);
-		//System.out.println("Number of Folder-->"+countFolder);
 		}
 		catch(Exception e){
 			//System.out.println("Exception Occured");
 			//System.out.println(e.getMessage());
 			//throw new ownException("msg");
-			//System.out.println("error msg");
+			System.out.println("error msg");
 		}
+return countFolder;
+}*/
+/*int countFiles(String path)throws ownException{
+	try{
+		File f_name=new File(path);
+		File[] listOfAllFile=f_name.listFiles();
+		for(int i=0;i<listOfAllFile.length;i++){
+			boolean res=listOfAllFile[i].isDirectory();
+			boolean res2=listOfAllFile[i].isFile();
+			if(res2==true){
+				//System.out.println(i+"->Directory-:>"+listOfAllFile[i]);
+				String s=listOfAllFile[i].getPath();
+				 countFile=countFile+1;
+			}
+			else if(res==true){
+				countFolder++;
+				String s=listOfAllFile[i].getPath();
+				countFiles(s);
+			}
+			else{
+				return 0;	
+			}
+		}
+		}
+		catch(Exception e){
+			System.out.println("error msg");
+		}
+return countFile;
+}*/
+void endsWithFiles(String path)throws Exception{
+	try{
+		File f_name=new File(path);
+		File[] listOfAllFile=f_name.listFiles();
+		for(int i=0;i<listOfAllFile.length;i++){
+			boolean isFile=listOfAllFile[i]. isFile();
+			boolean isDir=listOfAllFile[i].isDirectory ();			
+
+			String s=listOfAllFile[i].getPath();
+				if(isFile==true){
+						String filePath=listOfAllFile[i].getPath();
+						boolean endsjava=filePath.endsWith(".java");
+						boolean endsClass=s.endsWith(".class");
+						if(endsjava==true){
+							countEndsWithJava++;
+							System.out.println(listOfAllFile[i]);
+						}
+						else if(endsClass==true){
+							//System.out.println(listOfAllFile[i]);
+							countEndsWithClass++;
+						}
+	
+
+				 else if(isDir==true){
+					System.out.println(listOfAllFile[i]);
+					countFolder++;
+					endsWithFiles(s);
+				}
+
+			}
+		}
+	}
+	catch(Exception e){
+		System.out.println("errMsg");
+	}
 }
-	public static void main(String args[])throws ownException{
+	public static void main(String args[])throws Exception{
 		CountFile_Folder obj1=new CountFile_Folder();
-		obj1.countMethod1("/home/dhanshri/java/java_practice/file");
-		System.out.println("Number of Files-->"+obj1.countFile);
-		System.out.println("Number of Folder-->"+obj1.countFolder);
+	//	obj1.countFolder("/home/dhanshri/java/java_practice/file");
+		obj1.endsWithFiles("/home/dhanshri/java/java_practice/file");
+		//System.out.println("Number of Files-->"+obj1.countFile);
+		System.out.println("Number of  java Files-->"+obj1.countEndsWithJava);
+		System.out.println("Number of  Class Files-->"+obj1.countEndsWithClass);
+		//System.out.println("Number of Folder-->"+obj1.countFolder);
+		/*obj1.endsWithJava("/home/dhanshri/java/java_practice/file");
+		System.out.println("Number of Files Ends With java-->"+obj1.countEndsWithJava);
+		System.out.println("Number of Files Ends With-->"+obj1.countEndsWithClass);*/
 	}
 }
 class ownException extends Exception{
