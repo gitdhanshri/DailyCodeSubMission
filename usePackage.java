ccc3cfbecb7d6424fa8577212cda0927c3a00a3d
gitdhanshri <2022bit049@sggs.ac.in>
2024-05-10 22:11:47 +0530

    Creating own package to perform add,sub,mul,div etc

---
+++ usePackage.java
@@ -0,0 +1,11 @@
+import AddPack.Operation;
+class Usepack{
+	public static void main(String args[]){
+	        Operation obj1=new Operation(11,13);
+		obj1.add();
+		obj1.sub();
+		obj1.mul();
+		obj1.div();
+		obj1.mul(9,9);
+	}
+}
