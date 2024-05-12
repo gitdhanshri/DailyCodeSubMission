58fd2d16fc84066058225b7511b07e486aa9f9d8
gitdhanshri <2022bit049@sggs.ac.in>
2024-05-12 23:43:46 +0530

    _2packHavingClassName

---
+++ _2PackHavingSameClassName/operation.java
@@ -0,0 +1,11 @@
+package pack1
+public class operation{
+	private int num1,num2;
+	operation(int n1,int n2){
+		this.num1=n1;
+		this.num2=n2;
+	}
+	void sum(){
+		System.out.println("Sum is-->"+(num1+num2));
+	}
+}
---
+++ _2PackHavingSameClassName/operation2.java
@@ -0,0 +1,18 @@
+package pack2;
+/*public class operation2{
+	private int num1,num2;
+	operation2(int n1,int n2){
+		this.num1=n1;
+		this.num2=n2;
+	}
+}*/
+public class operation{
+	private int num1,num2;
+	operation(int n1,int n2){
+		this.num1=n1;
+		this.num2=n2;
+	}
+	void sum(){
+		System.out.println("Sum is-->"+(num1+num2));
+	}
+}
